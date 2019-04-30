260c7a878efd97bbb441c5588a98f1b7c2f508b4
ecrucru <ecrucru@users.noreply.github.com>
2019-04-30 19:12:26 +0200

    Copy engine ELO to new game

--- lib/pychess/Players/engineNest.py
+++ lib/pychess/Players/engineNest.py
@@ -457,8 +457,8 @@ class EngineDiscoverer(GObject.GObject):
                     result["protover"] = 2
                 if engine.protocol == "uci":
                     result["analyze"] = True
-                # if engine.elo > 0:
-                #     result["elo"] = engine.elo
+                if engine.elo > 0:
+                    result["elo"] = engine.elo
 
                 # Attached interpreter
                 ext = os.path.splitext(name.lower())[1]
